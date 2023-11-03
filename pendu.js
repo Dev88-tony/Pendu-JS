d4e19c428d6c847e9b823ccbdefb274525a238d7
Tony Bassiere <123980991+Dev88-tony@users.noreply.github.com>
2023-11-03 16:02:51 +0100

    v1

--- pendu.js
+++ pendu.js
@@ -1,16 +1,44 @@
-// Utilisation de la méthode fetch pour charger le fichier JSON de manière asynchrone
-fetch("mots.json")
-  .then(response => response.json()) // Conversion de la réponse en objet JavaScript
-  .then(data => {
-    // data contient maintenant les données du fichier JSON
-    console.log(data); // Affiche le contenu du fichier JSON dans la console
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+const mots = [
+      "ordinateur", "banane", "chocolat", "maison", "voiture", "football", "fraise", "girafe", "television", "hamburger", "chat",
+      "chien", "ecole", "avion", "soleil", "lune", "mer", "montagne", "forêt", "fleur", "guitare", "piano", "theâtre", "fromage", 
+      "ciseaux", "telephone", "livre", "peinture", "danse", "musee", "montre", "jupe", "cravate", "echarpe", "portable", "stylo", 
+      "fenêtre", "porte", "table", "chaise", "lit", "bateau", "piscine", "velo", "train", "bus", "metro", "tramway", "marteau", "cle",
+      "serrure", "fenêtre", "pain", "sandwich", "pomme", "orange", "poire", "pastèque", "cerise", "abricot", "pêche", "ananas", "kiwi",
+      "fraise", "framboise", "noix", "amande", "noisette", "avocat", "courgette", "tomate", "poivron", "aubergine", "oignon", "ail", "poireau",
+      "carotte", "chou", "salade", "radis" ];
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+function motAleatoire() { 
+  return mots = Math.floor(Math.random() * mots.length);
+}
+////////////////////////////////////////////////////////////////
+// Selectionnez tous les elements avec la classe "alpha"
+const alphabet = document.querySelectorAll(".alpha");
+const ToucheFaite = document.getElementById("ToucheFaite");
+const coups = document.getElementById("coups");
+const lettreATrouver = document.getElementById("lettreATrouver");
 
-    // Vous pouvez accéder aux mots comme ceci
-    var mots = data.mots;
+let lettres = '';
+let nbCoups = 6;
 
-    // Exemple : affichage du premier mot
-    console.log("Premier mot : " + mots[0]);
-  })
-  .catch(error => {
-    console.error('Une erreur s\'est produite lors du chargement du fichier JSON : ', error);
+lettreATrouver.textContent = mots[8];
+
+alphabet.forEach(alpha => {
+  alpha.addEventListener('click', function() {
+    if (lettres.length < nbCoups) {
+      const lettre = alpha.textContent;
+      lettres += lettre;
+      ToucheFaite.textContent = `${lettres}`;
+    } else {
+      coups.textContent = 'Vous êtes Pendu ! ';
+    }
   });
+});
+
+
+
+console.log(mots);
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
