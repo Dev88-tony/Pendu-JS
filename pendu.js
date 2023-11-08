32d11f861582038d492adad44ec69963685fd87b
Tony Bassiere <123980991+Dev88-tony@users.noreply.github.com>
2023-11-08 16:21:53 +0100

    v1

--- pendu.js
+++ pendu.js
@@ -1,22 +1,22 @@
 ////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
 const mots = [
-      "ordinateur", "banane", "chocolat", "maison", "voiture", "football", "fraise", "girafe", "television", "hamburger", "chat",
-      "chien", "ecole", "avion", "soleil", "lune", "mer", "montagne", "foret", "fleur", "guitare", "piano", "theatre", "fromage", 
-      "ciseaux", "telephone", "livre", "peinture", "danse", "musee", "montre", "jupe", "cravate", "echarpe", "portable", "stylo", 
-      "fenetre", "porte", "table", "chaise", "lit", "bateau", "piscine", "velo", "train", "bus", "metro", "tramway", "marteau", "cle",
-      "serrure", "fenetre", "pain", "sandwich", "pomme", "orange", "poire", "pasteque", "cerise", "abricot", "peche", "ananas", "kiwi",
-      "fraise", "framboise", "noix", "amande", "noisette", "avocat", "courgette", "tomate", "poivron", "aubergine", "oignon", "ail", "poireau",
-      "carotte", "chou", "salade", "radis" ];
+  "ordinateur", "banane", "chocolat", "maison", "voiture", "football", "fraise", "girafe", "television", "hamburger", "chat",
+  "chien", "ecole", "avion", "soleil", "lune", "mer", "montagne", "foret", "fleur", "guitare", "piano", "theatre", "fromage", 
+  "ciseaux", "telephone", "livre", "peinture", "danse", "musee", "montre", "jupe", "cravate", "echarpe", "portable", "stylo", 
+  "fenetre", "porte", "table", "chaise", "lit", "bateau", "piscine", "velo", "train", "bus", "metro", "tramway", "marteau", "cle",
+  "serrure", "fenetre", "pain", "sandwich", "pomme", "orange", "poire", "pasteque", "cerise", "abricot", "peche", "ananas", "kiwi",
+  "fraise", "framboise", "noix", "amande", "noisette", "avocat", "courgette", "tomate", "poivron", "aubergine", "oignon", "ail", "poireau",
+  "carotte", "chou", "salade", "radis" ];
 ////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
 const images = [ 
-  './Images/1.webp', 
-  './Images/2.webp', 
-  './Images/3.webp', 
-  './Images/4.webp', 
-  './Images/5.webp', 
-  './Images/6.webp' ];
+'./Images/1.webp', 
+'./Images/2.webp', 
+'./Images/3.webp', 
+'./Images/4.webp', 
+'./Images/5.webp', 
+'./Images/6.webp' ];
 ////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
 
@@ -32,57 +32,57 @@ let lettres = "";
 let nbCoups = 6;
 
 btn.addEventListener('click', function() {
-  reJouer();
+reJouer();
 });
 
 alphabet.forEach(alpha => {
-  alpha.addEventListener('click', function() {
-    if (lettres.length < nbCoups) {
-      const [...lettre] = alpha.textContent;
-      lettres += lettre;
-      lettreATrouver.textContent = afficherTirets();
-      coups = afficherCoups();
-    } else {
-      coups.textContent = `Vous êtes Pendu ! Le mot était ${motAlea}`;
-    }
-  });
+alpha.addEventListener('click', function() {
+if (lettres.length < nbCoups) {
+  const [...lettre] = alpha.textContent;
+  lettres += lettre;
+  lettreATrouver.textContent = afficherTirets();
+  coups = afficherCoups();
+} else {
+  coups.textContent = `Vous êtes Pendu ! Le mot était ${motAlea}`;
+}
+});
 });
 
 function afficherCoups() {
-  let coupsRestants = nbCoups - lettres.length;
-  if (coupsRestants >= 0) {
-    document.getElementById('Image').src = images[6 - coupsRestants - 1];
-    return coupsRestants + 1;
-  } else {
-    document.getElementById('Image').src = images[0];
-    return '';
-  }
+let coupsRestants = nbCoups - lettres.length;
+if (coupsRestants >= 0) {
+document.getElementById('Image').src = images[6 - coupsRestants - 1];
+return coupsRestants + 1;
+} else {
+document.getElementById('Image').src = images[0];
+return '';
+}
 }
 
 function afficherTirets() {
-  let tirets = "";
-  for (let i = 0; i < motAlea.length; i++) {
-    tirets += "_ ";
-  }
-  return tirets;
+let tirets = "";
+for (let i = 0; i < motAlea.length; i++) {
+tirets += "_ ";
+}
+return tirets;
 }
 
 function motAleatoire(mots) { 
-  const indexAleatoire = Math.floor(Math.random() * mots.length);
-  return mots[indexAleatoire];
+const indexAleatoire = Math.floor(Math.random() * mots.length);
+return mots[indexAleatoire];
 }
 console.log(motAlea);
 
 function reJouer() {
-  lettres = "";
-  motAlea = motAleatoire(mots);
-  coups.textContent = "";
-  lettreATrouver.textContent = afficherTirets();
-  document.getElementById('Image').src = image;
+lettres = "";
+motAlea = motAleatoire(mots);
+coups.textContent = "";
+lettreATrouver.textContent = afficherTirets();
+document.getElementById('Image').src = image;
 
-  alphabet.forEach(alpha => {
-    alpha.disabled = false;
-  });
+alphabet.forEach(alpha => {
+alpha.disabled = false;
+});
 }
 ////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
